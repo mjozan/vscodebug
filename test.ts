75dd2b3805657cad9d4f7f6ded84607ff1ce7df6
martin <martinjozan@gmail.com>
2020-06-27 04:26:25 +0200

    second commit

--- test.ts
+++ test.ts
@@ -1,6 +1,9 @@
 import { Mongoose } from "mongoose";
-import * as truc from "vscode";
-console.log(truc);
-Mongoose.apply("iuhiu");
-// let truc = new ExtensionContext();
-console.log("yuiyiu");
+import * as _ from "lodash";
+import * as test from "vscode";
+const randomInt = require("random-int");
+console.log(test);
+console.log(randomInt(10, 100));
+const numbers = [1, 5, 8, 10, 1, 5, 15, 42, 5];
+const uniqNumbers = _.uniq(numbers);
+console.log(uniqNumbers);
