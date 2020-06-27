4d45ad94efa00b4b0b2d0c7e1c65eca326824b24
martin <martinjozan@gmail.com>
2020-06-27 03:56:55 +0200

    initial commit

---
+++ test.js
@@ -0,0 +1,6 @@
+var mongoose_1 = require("mongoose");
+var truc = require("vscode");
+console.log(truc);
+mongoose_1.Mongoose.apply("iuhiu");
+// let truc = new ExtensionContext();
+console.log("yuiyiu");
---
+++ test.ts
@@ -0,0 +1,6 @@
+import { Mongoose } from "mongoose";
+import * as truc from "vscode";
+console.log(truc);
+Mongoose.apply("iuhiu");
+// let truc = new ExtensionContext();
+console.log("yuiyiu");
